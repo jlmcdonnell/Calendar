8761ab1c6b959e94432ac5cfe58159529f603993
John McDonnell <john+git@mcd.dev>
2023-08-01 09:21:38 +0100

    Use exitProcess

--- app/src/main/java/dev/mcd/calendar/ui/common/extensions/ContextExtensions.kt
+++ app/src/main/java/dev/mcd/calendar/ui/common/extensions/ContextExtensions.kt
@@ -2,10 +2,11 @@ package dev.mcd.calendar.ui.common.extensions
 
 import android.content.Context
 import android.content.Intent
+import kotlin.system.exitProcess
 
 fun Context.restartApplication() {
     val intent = packageManager.getLaunchIntentForPackage(packageName)!!
     val mainIntent = Intent.makeRestartActivityTask(intent.component)
     startActivity(mainIntent)
-    Runtime.getRuntime().exit(0)
+    exitProcess(0)
 }
